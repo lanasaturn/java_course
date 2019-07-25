0e01ed0bf77af8d8ffec4a3d399ad7ad3b13d282
lanasaturn <lanasaturn@gmail.com>
2019-07-25 17:31:17 +0300

    пампам

--- sandbox/src/main/java/ru/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
 }
