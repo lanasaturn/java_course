6e82a474f558d73bd183f090aee8b233cd05d8ba
lanasaturn <53267139+lanasaturn@users.noreply.github.com>
2019-07-26 17:07:38 +0300

    L1_10

--- sandbox/src/main/java/test/qa/MyFirstProgram.java
+++ sandbox/src/main/java/test/qa/MyFirstProgram.java
@@ -2,13 +2,10 @@ package test.qa;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 - 2);
-        System.out.println(1.000 / 2);
-        System.out.println("rrr" + 2);
-		System.out.println(1/2.0);
-		System.out.println((2*2)+"2");
+        System.out.println("Hello " + "your fucking " + "world");
+        int a = 6;
+        int s = a * a;
+        System.out.println("Площадь квадрата со стороной " + a + " = " +s);
+
     }
 }
