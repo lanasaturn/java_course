e1c507e57fa8e73c9f1e7771974a983c408cee70
lanasaturn <53267139+lanasaturn@users.noreply.github.com>
2019-07-26 16:57:45 +0300

    Л1_9

---
+++ sandbox/src/main/java/test/qa/MyFirstProgram.java
@@ -0,0 +1,14 @@
+package test.qa;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 - 2);
+        System.out.println(1.000 / 2);
+        System.out.println("rrr" + 2);
+		System.out.println(1/2.0);
+		System.out.println((2*2)+"2");
+    }
+}
