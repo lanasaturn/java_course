ba5f680002f87acd8bc9052f1a0d2bb840b36980
lanasaturn <53267139+lanasaturn@users.noreply.github.com>
2019-07-29 10:18:54 +0300

    Первый модуль полный

--- sandbox/build/scripts/sandbox.bat
+++ sandbox/build/scripts/sandbox.bat
@@ -17,7 +17,7 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem  sandbox startup script for Windows
 @rem
 @rem ##########################################################################
 
@@ -27,10 +27,10 @@ if "%OS%"=="Windows_NT" setlocal
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+set APP_HOME=%DIRNAME%..
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+@rem Add default JVM options here. You can also use JAVA_OPTS and SANDBOX_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -79,19 +79,19 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%APP_HOME%\lib\sandbox.jar
 
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+@rem Execute sandbox
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SANDBOX_OPTS%  -classpath "%CLASSPATH%" MyFirstProgram %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem Set variable SANDBOX_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+if  not "" == "%SANDBOX_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
--- sandbox/src/main/java/test/qa/MyFirstProgram.java
+++ sandbox/src/main/java/test/qa/MyFirstProgram.java
@@ -12,7 +12,18 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(5,6);
         System.out.println("Прямоугольник: "+ r.a +" и  "+ r.b +". Площадь = " + r.area() );
 
-
+        System.out.println();
+        System.out.println("Задачка про точки");
+        Point p1 = new Point(7,3);
+        Point p2 = new Point(2.,6);
+        System.out.println("Вариант  1:");
+        System.out.println(Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2)));
+        System.out.println();
+        System.out.println("Вариант 2:");
+        System.out.println(distance(p1, p2));
+        System.out.println();
+        System.out.println("Вариант 3:");
+        System.out.println(p1.distance(p2));
 
     }
 
@@ -24,12 +35,10 @@ public class MyFirstProgram {
     public static void hello2(String smb){
         System.out.println("Hello " + smb);
     }
-
-
-
-
-
-
-
+    public static double distance(Point p1, Point p2){
+        double m1 = p2.x - p1.x;
+        double m2 = p2.y - p1.y;
+        return(Math.sqrt(Math.pow(m1, 2) + Math.pow(m2, 2)));
+    }
 
 }
